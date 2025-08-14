910ce2970682908cf50ecbb8a1c2a4845e9ee2db
Elisabeth Røysland <elisabeth.skartveit@lyse.net>
2025-08-14 15:39:12 +0200

    Elisabeth Røysland

--- api-counter.wwwapi9/Program.cs
+++ api-counter.wwwapi9/Program.cs
@@ -28,33 +28,62 @@ var counters = app.MapGroup("/counters");
 //TODO: 1. write a method that returns all counters in the counters list.  use method below as a starting point
 counters.MapGet("/", () =>
 {
-    return TypedResults.Ok();
+    return TypedResults.Ok(CounterHelper.Counters);
 });
 
 
 //TODO: 2. write a method to return a single counter based on the id being passed in.  complete method below
 counters.MapGet("/{id}", (int id) =>
 {
-    return TypedResults.Ok(id);
+    return TypedResults.Ok(CounterHelper.Counters.FirstOrDefault(x => x.Id == id));
 });
 
 //TODO: 3.  write another method that returns counters that have a value greater than the {number} passed in.        
 counters.MapGet("/greaterthan/{number}", (int number) =>
 {
-    return TypedResults.Ok(number);
+    return TypedResults.Ok(CounterHelper.Counters.FindAll(x => x.Value > number));
 });
 
 ////TODO:4. write another method that returns counters that have a value less than the {number} passed in.
+counters.MapGet("/lessthan/{number}", (int number) =>
+{
+    return TypedResults.Ok(CounterHelper.Counters.FindAll(x => x.Value < number));
+});
 
 //Extension #1
 //TODO:  1. Write a controller method that increments the Value property of a counter of any given Id.
 //e.g.  with an Id=1  the Books counter Value should be increased from 5 to 6
 //return the counter you have increased
+counters.MapGet("/incrementsValue/{id}", (int id) =>
+{
+    var entity = CounterHelper.Counters.FirstOrDefault(x => x.Id == id);
+
+    if (entity != null)
+    {
+        entity.Value += 1;
+        return Results.Ok(entity);
+    }
+
+    return Results.NotFound();
+});
+
 
 //Extension #2
 //TODO: 2. Write a controller method that decrements the Value property of a counter of any given Id.
 //e.g.  with an Id=1  the Books counter Value should be decreased from 5 to 4
 //return the counter you have decreased
+counters.MapGet("/decrementsValue/{id}", (int id) =>
+{
+    var entity = CounterHelper.Counters.FirstOrDefault(x => x.Id == id);
+
+    if (entity != null)
+    {
+        entity.Value -= 1;
+        return Results.Ok(entity);
+    }
+
+    return Results.NotFound();
+});
 
 
 //Super Optional Extension #1 - Refactor the code!
